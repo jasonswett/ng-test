93f0d0570ee8d566a5a628fedc79ebf489e0fbe2
Jason Swett <jason@benfranklinlabs.com>
2014-10-21 19:05:51 -0400

    Add ng-token-auth module.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -26,6 +26,7 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/ng-token-auth/dist/ng-token-auth.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js'
